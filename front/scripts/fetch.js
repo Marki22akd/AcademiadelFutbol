a6e10ea994d5e3338d8a23c3895926db576d711f
agustinlehmann <agustinlehmann20@gmail.com>
2024-09-27 19:21:45 -0300

    vercel

--- front/scripts/fetch.js
+++ front/scripts/fetch.js
@@ -1,4 +1,4 @@
-const proxy = "http://localhost:3000/api/"
+const proxy = "https://academiadel-futbol-api.vercel.app/api"
 const options = {method: 'GET', headers: {accept: 'application/json'}}
 const token = "yeXvVqwSmC75alL2n3hb91GOOcrwjxbV9fmT3ukg"
 //const token = "pHjYLRNeNNyslOOnQAC097XvHMM30x0XBSMtEtqa"
