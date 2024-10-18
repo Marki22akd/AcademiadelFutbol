a53abb12419c9f3065fa08c962901ae47777731c
Marki22akd <166182973+Marki22akd@users.noreply.github.com>
2024-10-18 15:39:39 -0300

    Update fetch.js

--- front/scripts/fetch.js
+++ front/scripts/fetch.js
@@ -4,6 +4,7 @@ const options = {method: 'GET', headers: {"Content-Type": "application/json",
     "Access-Control-Allow-Credentials": "true",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Access-Control-Request-Method": "GET, POST, PUT, DELETE, PATCH, OPTIONS",}}
-const token = "yeXvVqwSmC75alL2n3hb91GOOcrwjxbV9fmT3ukg"
+const token = "5ZoK7lydmySsApLqstwUw3QZEoGoBFkOz5AZbiAs"
+//const token = "yeXvVqwSmC75alL2n3hb91GOOcrwjxbV9fmT3ukg"
 //const token = "pHjYLRNeNNyslOOnQAC097XvHMM30x0XBSMtEtqa"
 //const token = "GrrzBMnZJc3W2NbVfNrlb4hjJlo01Xl2WOWHlRwc"
